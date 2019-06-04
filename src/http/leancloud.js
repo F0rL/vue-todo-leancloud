942e95febf7b714cbd5f993fecbb1c0b83c0617c
Forl <kumaloveslife@gmail.com>
2019-06-04 14:21:45 +0800

    初始化 无后端分支

--- src/http/leancloud.js
+++ src/http/leancloud.js
@@ -8,33 +8,3 @@ AV.init({
   appKey: APP_KEY
 });
 
-class Leancloud {
-  constructor(className) {
-    this.className = className
-  }
-
-  create(data, dependentClassName, dependentId) {
-    if(!data) {
-      return Promise.reject('必须提供data数据')
-    }
-    if(typeof data !== 'object') {
-      return Promise.reject('参数data必须为对象')
-    }
-    let entries = Object.entries(data)
-    let instance = new AV.Object(this.className)
-    if(dependentClassName && dependentId) {
-      //本地构建一个 AV.Object 的方式，通过接口和 objectId 把数据从云端拉取到本地
-      let dependentInstance = AV.Object(dependentClassName, dependentId)
-      instance.set('dependent', dependentInstance)
-    }
-    entries.forEach(array => {
-      instance.set(array[0], array[1])
-    })
-    console.log('create todo')
-    return instance.save()
-  }
-}
-
-export default Leancloud
-
-
--- src/main.js
+++ src/main.js
@@ -3,13 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-//不使用VueX和后端
-//import Leancloud from './http/leancloud-lower'
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
