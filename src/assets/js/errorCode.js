008f6cb04a1f10d88d1bc5a32f79d82ad0a92b5b
Forl <kumaloveslife@gmail.com>
2019-06-16 23:12:11 +0800

    抽离错误码处理

---
+++ src/assets/js/errorCode.js
@@ -0,0 +1,17 @@
+export default function (error) {
+  let code = error.code
+  switch (code) {
+    case 202:
+      return '用户名已经被占用';
+    case 203:
+      return '电子邮箱地址已经被占用';
+    case 210:
+      return '用户名和密码不匹配';
+    case 211:
+      return '找不到用户';
+    case 219:
+      return '登录失败次数超过限制，请稍候再试';
+    default:
+      return '未知错误'
+  }
+}
